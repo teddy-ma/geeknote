e88203522aed9ab0089845366dca7abffbb15fc2
Ma Lucheng <mlc880926@gmail.com>
2013-10-13 10:19:02 +0800

    who am i

--- lib/geeknote/client.rb
+++ lib/geeknote/client.rb
@@ -2,11 +2,8 @@ module GeekNote
   class Client
     def list_notebooks
       auth_token = "S=s1:U=840c1:E=148e577e596:C=1418dc6b999:P=1cd:A=en-devtoken:V=2:H=f597a57e759dfaba5e319931fcad97a2"
-
       client = EvernoteOAuth::Client.new(token: auth_token)
-
       note_store = client.note_store
-
       notebooks = note_store.listNotebooks
       puts "your note books list ......"
       notebooks.each do |nb|
@@ -19,18 +16,21 @@ module GeekNote
     end
 
     def list_tags
-
       auth_token = "S=s1:U=840c1:E=148e577e596:C=1418dc6b999:P=1cd:A=en-devtoken:V=2:H=f597a57e759dfaba5e319931fcad97a2"
-
       client = EvernoteOAuth::Client.new(token: auth_token)
-
       note_store = client.note_store
-
       tags = note_store.listTags
       puts "your tags list ......"
       tags.each do |tag|
         puts tag.name
       end
+    end
+
+    def who_am_i
+      auth_token = "S=s1:U=840c1:E=148e577e596:C=1418dc6b999:P=1cd:A=en-devtoken:V=2:H=f597a57e759dfaba5e319931fcad97a2"
+      client = EvernoteOAuth::Client.new(token: auth_token)
+      user_store = client.user_store
+      p user_store.getUser.username
     end
   end
 end
