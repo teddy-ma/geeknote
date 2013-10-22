b4be4a412a6af100e57afdd9520c400793ac2f92
Ma Lucheng <mlc880926@gmail.com>
2013-10-22 17:37:53 +0800

    amazing, I can create note in command line

--- lib/geeknote/client.rb
+++ lib/geeknote/client.rb
@@ -2,25 +2,38 @@ module GeekNote
   class Client
 
     @@evernoteHost = "sandbox.evernote.com" #www.evernote.com for production
+    @@auth_token = "S=s1:U=840c1:E=148e577e596:C=1418dc6b999:P=1cd:A=en-devtoken:V=2:H=f597a57e759dfaba5e319931fcad97a2"
 
-    def list_notebooks
-      auth_token = "S=s1:U=840c1:E=148e577e596:C=1418dc6b999:P=1cd:A=en-devtoken:V=2:H=f597a57e759dfaba5e319931fcad97a2"
-      client = EvernoteOAuth::Client.new(token: auth_token)
+
+    def get_token 
+      file = File.new(Dir.home+"/.geeknote", "r")
+      token = file.gets
+      file.close
+      return token
+    end
+
+
+    def list_notebooks      
+      client = EvernoteOAuth::Client.new(token: get_token, consumer_key:"mlc880926-8889", consumer_secret:"a298b6c359007305", sandbox: true)
       note_store = client.note_store
       notebooks = note_store.listNotebooks
       puts "your note books list ......"
       notebooks.each do |nb|
         puts nb.name
       end
-      # note = Evernote::EDAM::Type::Note.new
-      # note.title = "hello from Codecademy"
-      # note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note>Hello Evernote<br/></en-note>'
-      # note_store.createNote(note)
+    end
+
+    def create_note(title, content)
+      client = EvernoteOAuth::Client.new(token: get_token, consumer_key:"mlc880926-8889", consumer_secret:"a298b6c359007305", sandbox: true)
+      note_store = client.note_store
+      note = Evernote::EDAM::Type::Note.new
+      note.title = title
+      note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note>Hello Evernote<br/></en-note>'
+      note_store.createNote(note)
     end
 
     def list_tags
-      auth_token = "S=s1:U=840c1:E=148e577e596:C=1418dc6b999:P=1cd:A=en-devtoken:V=2:H=f597a57e759dfaba5e319931fcad97a2"
-      client = EvernoteOAuth::Client.new(token: auth_token)
+      client = EvernoteOAuth::Client.new(token: get_token, consumer_key:"mlc880926-8889", consumer_secret:"a298b6c359007305", sandbox: true)
       note_store = client.note_store
       tags = note_store.listTags
       puts "your tags list ......"
@@ -67,5 +80,6 @@ module GeekNote
       end
       File.open(Dir.home+"/.geeknote", 'w') { |file| file.write(final_token.token) }
     end
+
   end
 end
